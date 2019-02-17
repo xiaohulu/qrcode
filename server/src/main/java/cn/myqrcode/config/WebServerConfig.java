09cd6523d0339ea69dbcc9a2981c42d5cfbb7487
xiaohulu <idocument@qq.com>
2019-02-17 16:08:29 +0800

    支持从 http 跳转到 https

---
+++ server/src/main/java/cn/myqrcode/config/WebServerConfig.java
@@ -0,0 +1,50 @@
+package cn.myqrcode.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebServerConfig {
+	
+	/**
+	 * https 配置，当用户访问 http 时能自动跳转到 https TODO: 正式发布到生产后证书直接放到 apache 服务器中，不在这里处理
+	 * 
+	 * @return
+	 */
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+		return tomcat;
+	}
+
+	/**
+	 * 以 http 启动，然后跳转到 https
+	 * 
+	 * @return
+	 */
+	private Connector createStandardConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setPort(80);
+		connector.setSecure(false);
+		connector.setRedirectPort(443);
+		return connector;
+	}
+}
